de4dae09f626f5737c1f8e9f19968ad7f5d15f4a
James Pearce <github@tripleodeon.com>
2023-08-25 11:12:48 -0400

    Working with schema API

--- App.js
+++ App.js
@@ -1,37 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createQueries, createStore } from 'tinybase';
+import { createQueries } from 'tinybase';
+import { useCreateQueries, useResultCell } from 'tinybase/lib/ui-react';
+import { createTestStore } from './api/testStore';
 import {
-  useCell,
-  useCreateQueries,
-  useCreateStore,
-  useResultCell,
-} from 'tinybase/lib/ui-react';
+  useCreateTestStore,
+  useTextCardSourceIconCell,
+} from './api/testStore-ui-react';
 
 export default function App() {
-  const store = useCreateStore(() =>
-    createStore()
-      .setTablesSchema({
-        textCard: {
-          sourceIcon: { type: 'string' },
-          sourceName: { type: 'string' },
-          category: { type: 'string' },
-          cardId: { type: 'string' },
-          heading: { type: 'string' },
-          subheading: { type: 'string' },
-        },
-      })
-      .setRow('textCard', 'rowid', {
-        sourceIcon: 'lalalala',
-        sourceName: 'lala',
-        category: 'la',
-        cardId: 'la',
-        heading: 'la',
-        subheading: 'la',
-      })
+  const testStore = useCreateTestStore(() =>
+    createTestStore().setTextCardRow('rowid', {
+      sourceIcon: 'lalalala',
+      sourceName: 'lala',
+      category: 'la',
+      cardId: 'la',
+      heading: 'la',
+      subheading: 'la',
+    })
   );
 
-  const queries = useCreateQueries(store, (store) =>
+  const queries = useCreateQueries(testStore.getStore(), (store) =>
     createQueries(store).setQueryDefinition(
       'query',
       'textCard',
@@ -40,13 +29,14 @@ export default function App() {
       }
     )
   );
-
+  console.log(testStore.getTables());
+  console.log(queries.getResultTable('query'));
   return (
     <View style={styles.container}>
       <Text>
         This is a Tinybase test
         {' - '}
-        {useCell('textCard', 'rowid', 'sourceIcon', store)}
+        {useTextCardSourceIconCell('rowid', testStore)}
         {' - '}
         {useResultCell('query', 'rowid', 'heading', queries)}
       </Text>
