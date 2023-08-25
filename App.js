050303a2bfea81bcade4602c8429e55dc874575f
James Pearce <github@tripleodeon.com>
2023-08-25 11:02:49 -0400

    Memoize store and queries creation

--- App.js
+++ App.js
@@ -1,47 +1,56 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { createQueries, createStore } from "tinybase";
-import { useCreateTestStore } from "./api/testStore-ui-react";
-import { createTestStore } from "./api/testStore";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createQueries, createStore } from 'tinybase';
+import {
+  useCell,
+  useCreateQueries,
+  useCreateStore,
+  useResultCell,
+} from 'tinybase/lib/ui-react';
 
 export default function App() {
-  const store = createTestStore();
+  const store = useCreateStore(() =>
+    createStore()
+      .setTablesSchema({
+        textCard: {
+          sourceIcon: { type: 'string' },
+          sourceName: { type: 'string' },
+          category: { type: 'string' },
+          cardId: { type: 'string' },
+          heading: { type: 'string' },
+          subheading: { type: 'string' },
+        },
+      })
+      .setRow('textCard', 'rowid', {
+        sourceIcon: 'lalalala',
+        sourceName: 'lala',
+        category: 'la',
+        cardId: 'la',
+        heading: 'la',
+        subheading: 'la',
+      })
+  );
 
-  const queries = createQueries(store);
-
-  // THE CODE BELOW WORKS:
-  // const store = createStore().setTablesSchema({
-  //   textCard: {
-  //     sourceIcon: { type: "string" },
-  //     sourceName: { type: "string" },
-  //     category: { type: "string" },
-  //     cardId: { type: "string" },
-  //     heading: { type: "string" },
-  //     subheading: { type: "string" },
-  //   },
-  // });
-
-  // store.setRow("textCard", "rowid", {
-  //   sourceIcon: "lalalala",
-  //   sourceName: "lala",
-  //   category: "la",
-  //   cardId: "la",
-  //   heading: "la",
-  //   subheading: "la",
-  // });
-
-  // const queries = createQueries(store);
-
-  // queries.setQueryDefinition("query", "textCard", ({ select }) => {
-  //   select("heading");
-  // });
-
-  // console.log("result", queries.getResultTable("query"));
+  const queries = useCreateQueries(store, (store) =>
+    createQueries(store).setQueryDefinition(
+      'query',
+      'textCard',
+      ({ select }) => {
+        select('heading');
+      }
+    )
+  );
 
   return (
     <View style={styles.container}>
-      <Text>This is a Tinybase test</Text>
-      <StatusBar style="auto" />
+      <Text>
+        This is a Tinybase test
+        {' - '}
+        {useCell('textCard', 'rowid', 'sourceIcon', store)}
+        {' - '}
+        {useResultCell('query', 'rowid', 'heading', queries)}
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -49,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
